7ea839a277eb772430c25323fb9bbc488cdf203d
Merydian <Tillfrankenbach@web.de>
2023-12-21 09:55:12 +0100

    refactor: run ruff

--- ORStools/gui/ORStoolsDialog.py
+++ ORStools/gui/ORStoolsDialog.py
@@ -46,7 +46,15 @@ from qgis.gui import QgsMapCanvasAnnotationItem
 
 from PyQt5.QtCore import QSizeF, QPointF, QCoreApplication, Qt, QUrl
 from PyQt5.QtGui import QIcon, QTextDocument
-from PyQt5.QtWidgets import QAction, QDialog, QApplication, QMenu, QMessageBox, QDialogButtonBox, QCompleter
+from PyQt5.QtWidgets import (
+    QAction,
+    QDialog,
+    QApplication,
+    QMenu,
+    QMessageBox,
+    QDialogButtonBox,
+    QCompleter,
+)
 
 from ORStools import (
     RESOURCE_PREFIX,
@@ -191,14 +199,17 @@ class ORStoolsDialogMain:
         provider_id = self.dlg.provider_combo.currentIndex()
         provider = configmanager.read_config()["providers"][provider_id]
         api_key = provider["key"]
-        if api_key is not '':
-            url = f'https://api.openrouteservice.org/geocode/autocomplete?api_key={api_key}&text={text}'
+        if api_key is not "":
+            url = f"https://api.openrouteservice.org/geocode/autocomplete?api_key={api_key}&text={text}"
             request = QgsBlockingNetworkRequest()
             error_code = request.get(QNetworkRequest(QUrl(url)))
 
             if error_code == QgsBlockingNetworkRequest.ErrorCode.NoError:
                 reply = request.reply()
-                suggest = [i['properties']['name'] for i in json.loads(reply.content().data().decode("utf-8"))['features']]
+                suggest = [
+                    i["properties"]["name"]
+                    for i in json.loads(reply.content().data().decode("utf-8"))["features"]
+                ]
                 print(suggest)
                 completer = QCompleter(suggest)
                 completer.setCaseSensitivity(Qt.CaseInsensitive)
@@ -219,7 +230,6 @@ class ORStoolsDialogMain:
                 Then enter the API key for openrouteservice provider in Web ► ORS Tools ► Provider Settings or the 
                 settings symbol in the main ORS Tools GUI, next to the provider dropdown.""",
             )
-
 
     def add_geocoded_items(self):
         pass
@@ -263,8 +273,16 @@ class ORStoolsDialogMain:
             )  # setting parent enables modal view
 
             # add connection to linedit text change
-            self.dlg.geocode_start.textChanged.connect(lambda: self.reload_geocode_completer(self.dlg.geocode_start, self.dlg.geocode_start.text()))
-            self.dlg.geocode_dest.textChanged.connect(lambda: self.reload_geocode_completer(self.dlg.geocode_dest, self.dlg.geocode_dest.text()))
+            self.dlg.geocode_start.textChanged.connect(
+                lambda: self.reload_geocode_completer(
+                    self.dlg.geocode_start, self.dlg.geocode_start.text()
+                )
+            )
+            self.dlg.geocode_dest.textChanged.connect(
+                lambda: self.reload_geocode_completer(
+                    self.dlg.geocode_dest, self.dlg.geocode_dest.text()
+                )
+            )
             # connect geocode add button
             # self.dlg.geocode_add.clicked.connect(self.add_geocoded_items)
 
