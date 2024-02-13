fbd9df4fd75fda1f43c643eedd699bb79232c48c
Merydian <Tillfrankenbach@web.de>
2024-02-13 11:52:37 +0100

    feat: add option to export order of optimization route points to directions_points_layer_proc

--- ORStools/proc/directions_points_layer_proc.py
+++ ORStools/proc/directions_points_layer_proc.py
@@ -26,7 +26,7 @@
  *                                                                         *
  ***************************************************************************/
 """
-
+from qgis._core import QgsVectorLayer, QgsFeature, QgsGeometry, QgsProject, QgsProcessingParameterBoolean
 from qgis.core import (
     QgsWkbTypes,
     QgsCoordinateReferenceSystem,
@@ -57,6 +57,7 @@ class ORSDirectionsPointsLayerAlgo(ORSBaseProcessingAlgorithm):
         self.IN_OPTIMIZE = "INPUT_OPTIMIZE"
         self.IN_MODE = "INPUT_MODE"
         self.IN_SORTBY = "INPUT_SORTBY"
+        self.EXPORT_ORDER = "EXPORT_ORDER"
         self.PARAMETERS = [
             QgsProcessingParameterFeatureSource(
                 name=self.IN_POINTS,
@@ -89,6 +90,10 @@ class ORSDirectionsPointsLayerAlgo(ORSBaseProcessingAlgorithm):
                 OPTIMIZATION_MODES,
                 defaultValue=None,
                 optional=True,
+            ),
+            QgsProcessingParameterBoolean(
+                self.EXPORT_ORDER,
+                self.tr("Export order of jobs")
             ),
         ]
 
@@ -171,6 +176,30 @@ class ORSDirectionsPointsLayerAlgo(ORSBaseProcessingAlgorithm):
                             response, profile, from_value=from_value
                         )
                     )
+
+                    # Export layer of points with optimization order
+                    export_value = self.parameterAsBool(parameters, self.EXPORT_ORDER, context)
+                    if export_value:
+                        items = list()
+                        for route in response['routes']:
+                            for i, step in enumerate(route['steps']):
+                                location = step['location']
+                                items.append(location)
+
+                        point_layer = QgsVectorLayer(
+                            "point?crs=epsg:4326&field=ID:integer", "Steps", "memory"
+                        )
+
+                        point_layer.updateFields()
+                        for idx, coords in enumerate(items):
+                            x, y = coords
+                            feature = QgsFeature()
+                            feature.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(x, y)))
+                            feature.setAttributes([idx])
+
+                            point_layer.dataProvider().addFeature(feature)
+                        QgsProject.instance().addMapLayer(point_layer)
+
                 else:
                     params = directions_core.build_default_parameters(
                         preference, point_list=points, options=options
