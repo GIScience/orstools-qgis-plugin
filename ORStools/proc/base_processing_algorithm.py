c79d8fcd2ef0a0296bef91746e099ef60fc85bff
Merydian <Tillfrankenbach@web.de>
2024-04-16 14:25:16 +0200

    docs: extend type hints for base_processing_algorithm.py

--- ORStools/proc/base_processing_algorithm.py
+++ ORStools/proc/base_processing_algorithm.py
@@ -53,7 +53,7 @@ from ..gui.directions_gui import _get_avoid_polygons
 class ORSBaseProcessingAlgorithm(QgsProcessingAlgorithm):
     """Base algorithm class for ORS algorithms"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """
         Default attributes used in all child classes
         """
@@ -90,7 +90,7 @@ class ORSBaseProcessingAlgorithm(QgsProcessingAlgorithm):
         """
         return self.ALGO_NAME
 
-    def shortHelpString(self):
+    def shortHelpString(self) -> str:
         """
         Displays the sidebar help in the algorithm window
         """
@@ -99,7 +99,7 @@ class ORSBaseProcessingAlgorithm(QgsProcessingAlgorithm):
         return read_help_file(algorithm=self.ALGO_NAME, locale=locale)
 
     @staticmethod
-    def helpUrl():
+    def helpUrl() -> str:
         """
         Will be connected to the Help button in the Algorithm window
         """
@@ -220,7 +220,7 @@ class ORSBaseProcessingAlgorithm(QgsProcessingAlgorithm):
         return options
 
     # noinspection PyUnusedLocal
-    def initAlgorithm(self, configuration):
+    def initAlgorithm(self, configuration) -> None:
         """
         Combines default and algorithm parameters and adds them in order to the
         algorithm dialog window.
@@ -242,6 +242,6 @@ class ORSBaseProcessingAlgorithm(QgsProcessingAlgorithm):
 
             self.addParameter(param)
 
-    def tr(self, string, context=None):
+    def tr(self, string: str, context=None) -> str:
         context = context or self.__class__.__name__
         return QCoreApplication.translate(context, string)
