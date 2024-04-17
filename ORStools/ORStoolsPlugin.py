eeaa64feed46b6072fa80622378afec1ba89fd0f
Till Frankenbach <81414045+merydian@users.noreply.github.com>
2024-04-17 09:02:49 +0200

    Update ORStools/ORStoolsPlugin.py
    Co-authored-by: Nyall Dawson <nyall.dawson@gmail.com>

--- ORStools/ORStoolsPlugin.py
+++ ORStools/ORStoolsPlugin.py
@@ -27,7 +27,7 @@
  ***************************************************************************/
 """
 
-from qgis._gui import QgisInterface
+from qgis.gui import QgisInterface
 from qgis.core import QgsApplication
 from PyQt5.QtCore import QTranslator, QSettings, qVersion, QCoreApplication
 import os.path
