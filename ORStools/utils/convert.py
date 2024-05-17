153449bb065ff9f8f9e71e95c828948c70f916d4
Jakob Schnell <Jakob.Schnell@heigit.org>
2024-05-17 10:14:49 +0200

    fix: correct type hint

--- ORStools/utils/convert.py
+++ ORStools/utils/convert.py
@@ -28,7 +28,7 @@
 """
 
 
-def decode_polyline(polyline: str, is3d: bool = False) -> dict:
+def decode_polyline(polyline: str, is3d: bool = False) -> list:
     """Decodes a Polyline string into a GeoJSON geometry.
 
     :param polyline: An encoded polyline, only the geometry.
