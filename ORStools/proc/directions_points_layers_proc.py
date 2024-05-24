4ed220e5871105c1d1e68a4984be3254eba95212
Merydian <Tillfrankenbach@web.de>
2024-05-24 16:13:37 +0200

    fix: add csv_colum parameter to request in ponts_layers_proc

--- ORStools/proc/directions_points_layers_proc.py
+++ ORStools/proc/directions_points_layers_proc.py
@@ -157,8 +157,9 @@ class ORSDirectionsPointsLayersAlgo(ORSBaseProcessingAlgorithm):
         options = self.parseOptions(parameters, context)
 
         csv_factor = self.parameterAsDouble(parameters, self.CSV_FACTOR, context)
+        csv_column = self.parameterAsString(parameters, self.CSV_COLUMN, context)
         if csv_factor > 0:
-            options["profile_params"] = {"weightings": {"csv_factor": csv_factor}}
+            options["profile_params"] = {"weightings": {"csv_factor": csv_factor, "csv_column": csv_column}}
 
         extra_info = self.parameterAsEnums(parameters, self.EXTRA_INFO, context)
         extra_info = [EXTRA_INFOS[i] for i in extra_info]
