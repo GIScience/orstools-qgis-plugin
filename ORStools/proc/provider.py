f4cd97c66f00558f05a4ed10e0b859d580a9b0bb
Till Frankenbach <Tillfrankenbach@web.de>
2024-05-27 05:26:03 -0400

    feat: add algorithm to qtoolbox

--- ORStools/proc/provider.py
+++ ORStools/proc/provider.py
@@ -38,6 +38,7 @@ from .directions_points_layers_proc import ORSDirectionsPointsLayersAlgo
 from .isochrones_layer_proc import ORSIsochronesLayerAlgo
 from .isochrones_point_proc import ORSIsochronesPointAlgo
 from .matrix_proc import ORSMatrixAlgo
+from .snap_proc import ORSSnapAlgo
 
 
 class ORStoolsProvider(QgsProcessingProvider):
@@ -63,6 +64,7 @@ class ORStoolsProvider(QgsProcessingProvider):
         self.addAlgorithm(ORSIsochronesLayerAlgo())
         self.addAlgorithm(ORSIsochronesPointAlgo())
         self.addAlgorithm(ORSMatrixAlgo())
+        self.addAlgorithm(ORSSnapAlgo())
 
     @staticmethod
     def icon() -> QIcon:
