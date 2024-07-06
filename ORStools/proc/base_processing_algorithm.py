fef537e968d55778021313fd6800b2b4c15a08e8
Till Frankenbach <Tillfrankenbach@web.de>
2024-07-06 15:36:31 -0400

    feat: actually remove advanced parameters from snap procs

--- ORStools/proc/base_processing_algorithm.py
+++ ORStools/proc/base_processing_algorithm.py
@@ -227,19 +227,19 @@ class ORSBaseProcessingAlgorithm(QgsProcessingAlgorithm):
         Combines default and algorithm parameters and adds them in order to the
         algorithm dialog window.
         """
-        if self.ALGO_NAME not in ["export_network_from_map"]:
+        if self.ALGO_NAME not in ["snap_from_point_layer", "snap_from_point", "export_network_from_map"]:
             parameters = (
-                [self.provider_parameter(), self.profile_parameter()]
-                + self.PARAMETERS
-                + self.option_parameters()
-                + [self.output_parameter()]
-            )
+            [self.provider_parameter(), self.profile_parameter()]
+            + self.PARAMETERS
+            + self.option_parameters()
+            + [self.output_parameter()]
+        )
         else:
             parameters = (
-                [self.provider_parameter(), self.profile_parameter()]
-                + self.PARAMETERS
-                + [self.output_parameter()]
-            )
+                    [self.provider_parameter(), self.profile_parameter()]
+                    + self.PARAMETERS
+                    + [self.output_parameter()]
+                    )
         for param in parameters:
             if param.name() in ADVANCED_PARAMETERS:
                 if self.GROUP == "Matrix":
