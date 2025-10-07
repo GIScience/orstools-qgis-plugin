99562b69268f535e9cb41c92030dec084d8a0767
Till Frankenbach <till.frankenbach@gmail.com>
2025-10-07 09:17:50 +0200

    refactor: update test cases to use create_qgs_field function

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -1,7 +1,7 @@
 import pytest
 from qgis.PyQt.QtCore import QVariant, QMetaType
 
-from ORStools.utils.wrapper import create_field_qgis_3_38_plus, create_field_legacy_qgis
+from ORStools.utils.wrapper import create_qgs_field
 
 test_cases_qvariant = [
     ("int_field", QVariant.Int, 10, 0, "Integer field", None),
@@ -19,24 +19,20 @@ test_cases_qmetatype = [
 @pytest.mark.parametrize(
     "name, type_enum, length, precision, comment, subtype_enum", test_cases_qvariant
 )
-def test_create_field_qgis_3_38_plus(name, type_enum, length, precision, comment, subtype_enum):
-    field = create_field_qgis_3_38_plus(name, type_enum, length, precision, comment, subtype_enum)
+def test_create_qgs_field_with_qvariant(name, type_enum, length, precision, comment, subtype_enum):
+    field = create_qgs_field(name, type_enum, length, precision, comment, subtype_enum)
     assert field.name() == name
     assert field.length() == length
     assert field.precision() == precision
     assert field.comment() == comment
-    assert field.type() == type_enum
-    assert field.subType() == (subtype_enum or QMetaType.Type.UnknownType)
 
 
 @pytest.mark.parametrize(
     "name, type_enum, length, precision, comment, subtype_enum", test_cases_qmetatype
 )
-def test_create_field_legacy_qgis(name, type_enum, length, precision, comment, subtype_enum):
-    field = create_field_legacy_qgis(name, type_enum, length, precision, comment, subtype_enum)
+def test_create_qgs_field_with_qmetatype(name, type_enum, length, precision, comment, subtype_enum):
+    field = create_qgs_field(name, type_enum, length, precision, comment, subtype_enum)
     assert field.name() == name
     assert field.length() == length
     assert field.precision() == precision
     assert field.comment() == comment
-    assert field.type() == type_enum
-    assert field.subType() == (subtype_enum or QVariant.Invalid)
