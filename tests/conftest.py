4742a85856fe88525c3543b8f20f35336bed6b33
Jakob Schnell <Jakob.Schnell@heigit.org>
2025-12-05 14:15:55 +0100

    fix: test setup (#365)
    * fix(tests): simplify setup
    
    * refactor(tests): move setup to session start

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,39 +3,22 @@ import os
 from qgis.core import QgsSettings
 
 from ORStools.ORStoolsPlugin import ORStools
-from ORStools.utils.configmanager import read_config
 from tests.utils.utilities import get_qgis_app
-
-QGISAPP, CANVAS, IFACE, PARENT = get_qgis_app()
-
-ORStools(IFACE).add_default_provider_to_settings()
-s = QgsSettings()
-data = s.value("ORStools/config")
-
 
 def pytest_sessionstart(session):
     """
     Called after the Session object has been created and
     before performing collection and entering the run test loop.
     """
+    QGISAPP, CANVAS, IFACE, PARENT = get_qgis_app()
+
+    ORStools(IFACE).add_default_provider_to_settings()
+    s = QgsSettings()
+    data = s.value("ORStools/config")
+
     if not os.environ.get("ORS_API_KEY"):
         raise ValueError(
             "No API key found in environment variables. Please set ORS_API_KEY environment variable to run tests."
         )
-    if data["providers"][0]["key"] == "":
-        data["providers"][0]["key"] = os.environ.get("ORS_API_KEY")
-        s.setValue("ORStools/config", data)
-    else:
-        raise ValueError("API key is not empty.")
-
-
-def pytest_sessionfinish(session, exitstatus):
-    """
-    Called after whole test run finished, right before
-    returning the exit status to the system.
-    """
-    if not data["providers"][0]["key"] == "":
-        data['providers'][0]['key'] = ''  # fmt: skip
+    data["providers"][0]["key"] = os.environ.get("ORS_API_KEY")
     s.setValue("ORStools/config", data)
-    config = read_config()
-    assert config["providers"][0]["key"] == ''  # fmt: skip
