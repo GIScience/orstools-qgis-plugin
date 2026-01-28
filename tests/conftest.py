33cbc96d6f4825a8d97cfb70832ac459d27ef986
Till Frankenbach <till.frankenbach@gmail.com>
2026-01-28 10:42:43 +0100

    fix: update providers on update of plugin

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import os
 
-from qgis.core import QgsSettings
 
 from ORStools.ORStoolsPlugin import ORStools
 from tests.utils.utilities import get_qgis_app
+from ORStools.utils import configmanager
 
 
 def pytest_sessionstart(session):
@@ -13,13 +13,13 @@ def pytest_sessionstart(session):
     """
     QGISAPP, CANVAS, IFACE, PARENT = get_qgis_app()
 
-    ORStools(IFACE).add_default_provider_to_settings()
-    s = QgsSettings()
-    data = s.value("ORStools/config")
+    ORStools(IFACE)
+
+    data = configmanager.read_config()
 
     if not os.environ.get("ORS_API_KEY"):
         raise ValueError(
             "No API key found in environment variables. Please set ORS_API_KEY environment variable to run tests."
         )
     data["providers"][0]["key"] = os.environ.get("ORS_API_KEY")
-    s.setValue("ORStools/config", data)
+    configmanager.write_config(data)
